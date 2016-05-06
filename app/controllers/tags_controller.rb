a31510ee36ba3dd90576b60e229e8330f3088a5c
佐々木巌 <itsuki.sasaki@gmail.com>
2016-05-07 02:03:55 +0900

    タグをクリックするによる検索一覧の表示 ・tags#showコントローラー、ビューの作成 ・videos#index、tags#indexからのlink_toメソッドによる検索一覧呼び出し

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,7 @@ class TagsController < ApplicationController
   def index
     @tags = ActsAsTaggableOn::Tag.most_used
   end
+  def show
+    @videos = Video.tagged_with(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :videos, only: [:index, :new, :create]
-  resources :tags, only: [:index]
+  resources :tags, only: [:index, :show]
   root to: "videos#index"
 end
